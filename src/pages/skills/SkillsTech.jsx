ba80391b01a41c80e888416e76df66146c8bc230
Herold7 <makpier@gmail.com>
2023-12-05 11:00:08 +0100

    🔥 suppression d'images

--- src/pages/skills/SkillsTech.jsx
+++ src/pages/skills/SkillsTech.jsx
@@ -7,43 +7,39 @@ function SkillsTech() {
             picture: '../../../projportf/HTML5.png',
             value: "80",
             type: "danger",
-            name:"Html",
         },
         {
             picture: '../../../projportf/css3.png',
             value: "70",
             type: "primary",
-            name:"Css",
         },
         {
             picture: '../../../projportf/js.png',
             value: "60",
             type: "warning",
-            name:"Js",
         },
         {
             picture: '../../../projportf/php.png',
             value: "50",
             type: "info",
-            name:"Php",
         },
         {
             picture: '../../../projportf/React.png',
             value: "60",
             type: "success",
-            name:"React",
+
         },
         {
             picture: '../../../projportf/Bootstrap.png',
             value: "60",
             type: "info",
-            name:"Bootstrap",
+
         },
         {
             picture: '../../../projportf/Symfony.png',
             value: "50",
             type: "danger",
-            name:"Symfony",
+
         },
     ];
 
@@ -52,43 +48,41 @@ function SkillsTech() {
             picture: '../../../projportf/vscode.png',
             value: "70",
             type: "primary",
-            name:"Vscode",
+
         },
         {
             picture: '../../../projportf/PhpStorm.png',
             value: "60",
             type: "success",
-            name:"PhpStorm",
+
         },
         {
             picture: '../../../projportf/mysqlWorkbench.png',
             value: "70",
             type: "danger",
-            name:"MysqlWorkbench",
+
         },
         {
             picture: '../../../projportf/git.png',
             value: "50",
             type: "info",
-            name:"Git",
         },
         {
             picture: '../../../projportf/github.png',
             value: "60",
             type: "warning",
-            name:"Github",
+
         },
         {
             picture: '../../../projportf/cms.png',
             value: "80",
             type: "success",
-            name:"Cms",
         },
         {
             picture: '../../../projportf/Figma.png',
             value: "60",
             type: "danger",
-            name:"Figma",
+
         },
     ];
 
@@ -97,7 +91,7 @@ function SkillsTech() {
             <div className="row" style={{ marginTop: '7rem' }}>
                 {skillsTech1.map((skill, index) => (
                     <div className="mt2 col-lg-12" key={index}>
-                        <h3 className={`m-3  text-${skill.type}`} style={{fontWeight:'bold'}}>{skill.name}</h3>
+                        <h3 className={`m-3  text-${skill.type}`} style={{ fontWeight: 'bold' }}></h3>
                         <div className="progress  mb-4" role="progressbar" style={{ height: '20px' }}
                             aria-label={`striped example 20px high${skill.type}`} aria-valuenow={skill.value} aria-valuemin="0"
                             aria-valuemax="100">
@@ -117,8 +111,8 @@ function SkillsTech() {
             <div className="row" style={{ marginTop: ' 10rem' }}>
                 {skillsTech2.map((skill, index) => (
                     <div className="mt2 col-lg-12 " key={index}>
-                        <h3 className={`m-3  text-${skill.type}`} style={{fontWeight:'bold'}}>{skill.name}</h3>
-                        <div className="progress  mb-4" role="progressbar" style={{ height: ' 20px' }}
+                        <h3 className={`m-3  text-${skill.type}`} style={{ fontWeight: 'bold' }}></h3>
+                        <div div className="progress  mb-4" role="progressbar" style={{ height: ' 20px' }}
                             aria-label={`striped example 20px high${skill.type}`} aria-valuenow={skill.value} aria-valuemin="0"
                             aria-valuemax="100">
                             <img className="technologie_skills" src={skill.picture} alt="logo_des_technologies" />
@@ -127,7 +121,7 @@ function SkillsTech() {
                         </div>
                     </div>
                 ))}
-            </div>
+            </div >
         </>
     )
 }
